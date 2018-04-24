db29975dc18f290f8411f4ee60ad7ad5c5228ba1
vahriin <systemd219@gmail.com>
2018-04-24 14:01:20 +0300

    add connection config

---
+++ connection.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type ConnectionConf struct {
+	DbName   string `json:"dbname"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+}
+
+func DefaultConnectionConf() ConnectionConf {
+	var cc ConnectionConf
+	cc.DbName = "sqltest"
+	cc.User = "sqltest_user"
+	cc.Password = ""
+	cc.Host = "localhost"
+	cc.Port = "5432"
+
+	return cc
+}
+
+func ReadConnectionConf(filename string) ConnectionConf {
+	confFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return DefaultConnectionConf()
+	}
+
+	jsonDecoder := json.NewDecoder(confFile)
+	var cc ConnectionConf
+	jsonDecoder.Decode(cc)
+	return cc
+}
